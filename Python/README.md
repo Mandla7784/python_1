873286df6b15093ebfffcb71adbe32b33dbba299
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-08-12 19:13:37 +0200

    Added a dictionary in main.py

--- Python/README.md
+++ Python/README.md
@@ -1,1 +1,3 @@
-## Python Learning Journey
+# Python Learning Journey
+
+## In this mini project i will be learning python.
--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,24 @@
-print("Hello Mandla")
+## python dictionaries 
+student = {"name":"John","age":"45" , "courses":[
+    "English","Math","Compsci"
+]
+,
+"friend":{
+ "name":"Mandla",
+ "age":22
+ }
+}
+my_friend = student.get("friend")
+for key,value in student.items():
+    if type(value == int):
+        print(value , value)
+    else:
+        print(key,value)
+   
+  
+     
+
+
+
+
+
